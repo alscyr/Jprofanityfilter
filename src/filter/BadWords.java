79e07eff7350a832e91ab282da0bda8299bc6764
Alex Schreyer <schreyer.alex@gmail.com>
2015-06-13 18:21:14 +0100

    Words are now stored internally and not in a text file

---
+++ src/filter/BadWords.java
@@ -0,0 +1,456 @@
+package filter;
+
+public class BadWords {
+
+public static String bad_words = "4r5e,\r\n" + 
+		"5h1t,\r\n" + 
+		"5hit,\r\n" + 
+		"a55,\r\n" + 
+		"anal,\r\n" + 
+		"anus,\r\n" + 
+		"ar5e,\r\n" + 
+		"arrse,\r\n" + 
+		"arse,\r\n" + 
+		"ass,\r\n" + 
+		"ass-fucker,\r\n" + 
+		"asses,\r\n" + 
+		"assfucker,\r\n" + 
+		"assfukka,\r\n" + 
+		"asshole,\r\n" + 
+		"assholes,\r\n" + 
+		"asswhole,\r\n" + 
+		"a_s_s,\r\n" + 
+		"b!tch,\r\n" + 
+		"b00bs,\r\n" + 
+		"b17ch,\r\n" + 
+		"b1tch,\r\n" + 
+		"ballbag,\r\n" + 
+		"balls,\r\n" + 
+		"ballsack,\r\n" + 
+		"bastard,\r\n" + 
+		"beastial,\r\n" + 
+		"beastiality,\r\n" + 
+		"bellend,\r\n" + 
+		"bestial,\r\n" + 
+		"bestiality,\r\n" + 
+		"bi+ch,\r\n" + 
+		"biatch,\r\n" + 
+		"bitch,\r\n" + 
+		"bitcher,\r\n" + 
+		"bitchers,\r\n" + 
+		"bitches,\r\n" + 
+		"bitchin,\r\n" + 
+		"bitching,\r\n" + 
+		"bloody,\r\n" + 
+		"blow job,\r\n" + 
+		"blowjob,\r\n" + 
+		"blowjobs,\r\n" + 
+		"boiolas,\r\n" + 
+		"bollock,\r\n" + 
+		"bollok,\r\n" + 
+		"boner,\r\n" + 
+		"boob,\r\n" + 
+		"boobs,\r\n" + 
+		"booobs,\r\n" + 
+		"boooobs,\r\n" + 
+		"booooobs,\r\n" + 
+		"booooooobs,\r\n" + 
+		"breasts,\r\n" + 
+		"buceta,\r\n" + 
+		"bugger,\r\n" + 
+		"bum,\r\n" + 
+		"bunny fucker,\r\n" + 
+		"butt,\r\n" + 
+		"butthole,\r\n" + 
+		"buttmuch,\r\n" + 
+		"buttplug,\r\n" + 
+		"c0ck,\r\n" + 
+		"c0cksucker,\r\n" + 
+		"carpet muncher,\r\n" + 
+		"cawk,\r\n" + 
+		"chink,\r\n" + 
+		"cipa,\r\n" + 
+		"cl1t,\r\n" + 
+		"clit,\r\n" + 
+		"clitoris,\r\n" + 
+		"clits,\r\n" + 
+		"cnut,\r\n" + 
+		"cock,\r\n" + 
+		"cock-sucker,\r\n" + 
+		"cockface,\r\n" + 
+		"cockhead,\r\n" + 
+		"cockmunch,\r\n" + 
+		"cockmuncher,\r\n" + 
+		"cocks,\r\n" + 
+		"cocksuck ,\r\n" + 
+		"cocksucked ,\r\n" + 
+		"cocksucker,\r\n" + 
+		"cocksucking,\r\n" + 
+		"cocksucks ,\r\n" + 
+		"cocksuka,\r\n" + 
+		"cocksukka,\r\n" + 
+		"cok,\r\n" + 
+		"cokmuncher,\r\n" + 
+		"coksucka,\r\n" + 
+		"coon,\r\n" + 
+		"cox,\r\n" + 
+		"crap,\r\n" + 
+		"cum,\r\n" + 
+		"cummer,\r\n" + 
+		"cumming,\r\n" + 
+		"cums,\r\n" + 
+		"cumshot,\r\n" + 
+		"cunilingus,\r\n" + 
+		"cunillingus,\r\n" + 
+		"cunnilingus,\r\n" + 
+		"cunt,\r\n" + 
+		"cuntlick ,\r\n" + 
+		"cuntlicker ,\r\n" + 
+		"cuntlicking ,\r\n" + 
+		"cunts,\r\n" + 
+		"cyalis,\r\n" + 
+		"cyberfuc,\r\n" + 
+		"cyberfuck ,\r\n" + 
+		"cyberfucked ,\r\n" + 
+		"cyberfucker,\r\n" + 
+		"cyberfuckers,\r\n" + 
+		"cyberfucking ,\r\n" + 
+		"d1ck,\r\n" + 
+		"damn,\r\n" + 
+		"dick,\r\n" + 
+		"dickhead,\r\n" + 
+		"dildo,\r\n" + 
+		"dildos,\r\n" + 
+		"dink,\r\n" + 
+		"dinks,\r\n" + 
+		"dirsa,\r\n" + 
+		"dlck,\r\n" + 
+		"dog-fucker,\r\n" + 
+		"doggin,\r\n" + 
+		"dogging,\r\n" + 
+		"donkeyribber,\r\n" + 
+		"doosh,\r\n" + 
+		"duche,\r\n" + 
+		"dyke,\r\n" + 
+		"ejaculate,\r\n" + 
+		"ejaculated,\r\n" + 
+		"ejaculates ,\r\n" + 
+		"ejaculating ,\r\n" + 
+		"ejaculatings,\r\n" + 
+		"ejaculation,\r\n" + 
+		"ejakulate,\r\n" + 
+		"f u c k,\r\n" + 
+		"f u c k e r,\r\n" + 
+		"f4nny,\r\n" + 
+		"fag,\r\n" + 
+		"fagging,\r\n" + 
+		"faggitt,\r\n" + 
+		"faggot,\r\n" + 
+		"faggs,\r\n" + 
+		"fagot,\r\n" + 
+		"fagots,\r\n" + 
+		"fags,\r\n" + 
+		"fanny,\r\n" + 
+		"fannyflaps,\r\n" + 
+		"fannyfucker,\r\n" + 
+		"fanyy,\r\n" + 
+		"fatass,\r\n" + 
+		"fcuk,\r\n" + 
+		"fcuker,\r\n" + 
+		"fcuking,\r\n" + 
+		"feck,\r\n" + 
+		"fecker,\r\n" + 
+		"felching,\r\n" + 
+		"fellate,\r\n" + 
+		"fellatio,\r\n" + 
+		"fingerfuck ,\r\n" + 
+		"fingerfucked ,\r\n" + 
+		"fingerfucker ,\r\n" + 
+		"fingerfuckers,\r\n" + 
+		"fingerfucking ,\r\n" + 
+		"fingerfucks ,\r\n" + 
+		"fistfuck,\r\n" + 
+		"fistfucked ,\r\n" + 
+		"fistfucker ,\r\n" + 
+		"fistfuckers ,\r\n" + 
+		"fistfucking ,\r\n" + 
+		"fistfuckings ,\r\n" + 
+		"fistfucks ,\r\n" + 
+		"flange,\r\n" + 
+		"fook,\r\n" + 
+		"fooker,\r\n" + 
+		"fuck,\r\n" + 
+		"fucka,\r\n" + 
+		"fucked,\r\n" + 
+		"fucker,\r\n" + 
+		"fuckers,\r\n" + 
+		"fuckhead,\r\n" + 
+		"fuckheads,\r\n" + 
+		"fuckin,\r\n" + 
+		"fucking,\r\n" + 
+		"fuckings,\r\n" + 
+		"fuckingshitmotherfucker,\r\n" + 
+		"fuckme ,\r\n" + 
+		"fucks,\r\n" + 
+		"fuckwhit,\r\n" + 
+		"fuckwit,\r\n" + 
+		"fudge packer,\r\n" + 
+		"fudgepacker,\r\n" + 
+		"fuk,\r\n" + 
+		"fuker,\r\n" + 
+		"fukker,\r\n" + 
+		"fukkin,\r\n" + 
+		"fuks,\r\n" + 
+		"fukwhit,\r\n" + 
+		"fukwit,\r\n" + 
+		"fux,\r\n" + 
+		"fux0r,\r\n" + 
+		"f_u_c_k,\r\n" + 
+		"gangbang,\r\n" + 
+		"gangbanged ,\r\n" + 
+		"gangbangs ,\r\n" + 
+		"gaylord,\r\n" + 
+		"gaysex,\r\n" + 
+		"goatse,\r\n" + 
+		"God,\r\n" + 
+		"god-dam,\r\n" + 
+		"god-damned,\r\n" + 
+		"goddamn,\r\n" + 
+		"goddamned,\r\n" + 
+		"hardcoresex ,\r\n" + 
+		"hell,\r\n" + 
+		"heshe,\r\n" + 
+		"hoar,\r\n" + 
+		"hoare,\r\n" + 
+		"hoer,\r\n" + 
+		"homo,\r\n" + 
+		"hore,\r\n" + 
+		"horniest,\r\n" + 
+		"horny,\r\n" + 
+		"hotsex,\r\n" + 
+		"jack-off ,\r\n" + 
+		"jackoff,\r\n" + 
+		"jap,\r\n" + 
+		"jerk-off ,\r\n" + 
+		"jism,\r\n" + 
+		"jiz ,\r\n" + 
+		"jizm ,\r\n" + 
+		"jizz,\r\n" + 
+		"kawk,\r\n" + 
+		"knob,\r\n" + 
+		"knobead,\r\n" + 
+		"knobed,\r\n" + 
+		"knobend,\r\n" + 
+		"knobhead,\r\n" + 
+		"knobjocky,\r\n" + 
+		"knobjokey,\r\n" + 
+		"kock,\r\n" + 
+		"kondum,\r\n" + 
+		"kondums,\r\n" + 
+		"kum,\r\n" + 
+		"kummer,\r\n" + 
+		"kumming,\r\n" + 
+		"kums,\r\n" + 
+		"kunilingus,\r\n" + 
+		"l3i+ch,\r\n" + 
+		"l3itch,\r\n" + 
+		"labia,\r\n" + 
+		"lmfao,\r\n" + 
+		"lust,\r\n" + 
+		"lusting,\r\n" + 
+		"m0f0,\r\n" + 
+		"m0fo,\r\n" + 
+		"m45terbate,\r\n" + 
+		"ma5terb8,\r\n" + 
+		"ma5terbate,\r\n" + 
+		"masochist,\r\n" + 
+		"master-bate,\r\n" + 
+		"masterb8,\r\n" + 
+		"masterbat*,\r\n" + 
+		"masterbat3,\r\n" + 
+		"masterbate,\r\n" + 
+		"masterbation,\r\n" + 
+		"masterbations,\r\n" + 
+		"masturbate,\r\n" + 
+		"mo-fo,\r\n" + 
+		"mof0,\r\n" + 
+		"mofo,\r\n" + 
+		"mothafuck,\r\n" + 
+		"mothafucka,\r\n" + 
+		"mothafuckas,\r\n" + 
+		"mothafuckaz,\r\n" + 
+		"mothafucked ,\r\n" + 
+		"mothafucker,\r\n" + 
+		"mothafuckers,\r\n" + 
+		"mothafuckin,\r\n" + 
+		"mothafucking ,\r\n" + 
+		"mothafuckings,\r\n" + 
+		"mothafucks,\r\n" + 
+		"mother fucker,\r\n" + 
+		"motherfuck,\r\n" + 
+		"motherfucked,\r\n" + 
+		"motherfucker,\r\n" + 
+		"motherfuckers,\r\n" + 
+		"motherfuckin,\r\n" + 
+		"motherfucking,\r\n" + 
+		"motherfuckings,\r\n" + 
+		"motherfuckka,\r\n" + 
+		"motherfucks,\r\n" + 
+		"muff,\r\n" + 
+		"mutha,\r\n" + 
+		"muthafecker,\r\n" + 
+		"muthafuckker,\r\n" + 
+		"muther,\r\n" + 
+		"mutherfucker,\r\n" + 
+		"n1gga,\r\n" + 
+		"n1gger,\r\n" + 
+		"nazi,\r\n" + 
+		"nigg3r,\r\n" + 
+		"nigg4h,\r\n" + 
+		"nigga,\r\n" + 
+		"niggah,\r\n" + 
+		"niggas,\r\n" + 
+		"niggaz,\r\n" + 
+		"nigger,\r\n" + 
+		"niggers ,\r\n" + 
+		"nob,\r\n" + 
+		"nob jokey,\r\n" + 
+		"nobhead,\r\n" + 
+		"nobjocky,\r\n" + 
+		"nobjokey,\r\n" + 
+		"numbnuts,\r\n" + 
+		"nutsack,\r\n" + 
+		"orgasim ,\r\n" + 
+		"orgasims ,\r\n" + 
+		"orgasm,\r\n" + 
+		"orgasms ,\r\n" + 
+		"p0rn,\r\n" + 
+		"pawn,\r\n" + 
+		"pecker,\r\n" + 
+		"penis,\r\n" + 
+		"penisfucker,\r\n" + 
+		"phonesex,\r\n" + 
+		"phuck,\r\n" + 
+		"phuk,\r\n" + 
+		"phuked,\r\n" + 
+		"phuking,\r\n" + 
+		"phukked,\r\n" + 
+		"phukking,\r\n" + 
+		"phuks,\r\n" + 
+		"phuq,\r\n" + 
+		"pigfucker,\r\n" + 
+		"pimpis,\r\n" + 
+		"piss,\r\n" + 
+		"pissed,\r\n" + 
+		"pisser,\r\n" + 
+		"pissers,\r\n" + 
+		"pisses ,\r\n" + 
+		"pissflaps,\r\n" + 
+		"pissin ,\r\n" + 
+		"pissing,\r\n" + 
+		"pissoff ,\r\n" + 
+		"poop,\r\n" + 
+		"porn,\r\n" + 
+		"porno,\r\n" + 
+		"pornography,\r\n" + 
+		"pornos,\r\n" + 
+		"prick,\r\n" + 
+		"pricks ,\r\n" + 
+		"pron,\r\n" + 
+		"pube,\r\n" + 
+		"pusse,\r\n" + 
+		"pussi,\r\n" + 
+		"pussies,\r\n" + 
+		"pussy,\r\n" + 
+		"pussys ,\r\n" + 
+		"rectum,\r\n" + 
+		"retard,\r\n" + 
+		"rimjaw,\r\n" + 
+		"rimming,\r\n" + 
+		"s hit,\r\n" + 
+		"s.o.b.,\r\n" + 
+		"sadist,\r\n" + 
+		"schlong,\r\n" + 
+		"screwing,\r\n" + 
+		"scroat,\r\n" + 
+		"scrote,\r\n" + 
+		"scrotum,\r\n" + 
+		"semen,\r\n" + 
+		"sex,\r\n" + 
+		"sh!+,\r\n" + 
+		"sh!t,\r\n" + 
+		"sh1t,\r\n" + 
+		"shag,\r\n" + 
+		"shagger,\r\n" + 
+		"shaggin,\r\n" + 
+		"shagging,\r\n" + 
+		"shemale,\r\n" + 
+		"shi+,\r\n" + 
+		"shit,\r\n" + 
+		"shitdick,\r\n" + 
+		"shite,\r\n" + 
+		"shited,\r\n" + 
+		"shitey,\r\n" + 
+		"shitfuck,\r\n" + 
+		"shitfull,\r\n" + 
+		"shithead,\r\n" + 
+		"shiting,\r\n" + 
+		"shitings,\r\n" + 
+		"shits,\r\n" + 
+		"shitted,\r\n" + 
+		"shitter,\r\n" + 
+		"shitters ,\r\n" + 
+		"shitting,\r\n" + 
+		"shittings,\r\n" + 
+		"shitty ,\r\n" + 
+		"skank,\r\n" + 
+		"slut,\r\n" + 
+		"sluts,\r\n" + 
+		"smegma,\r\n" + 
+		"smut,\r\n" + 
+		"snatch,\r\n" + 
+		"son-of-a-bitch,\r\n" + 
+		"spac,\r\n" + 
+		"spunk,\r\n" + 
+		"s_h_i_t,\r\n" + 
+		"t1tt1e5,\r\n" + 
+		"t1tties,\r\n" + 
+		"teets,\r\n" + 
+		"teez,\r\n" + 
+		"testical,\r\n" + 
+		"testicle,\r\n" + 
+		"tit,\r\n" + 
+		"titfuck,\r\n" + 
+		"tits,\r\n" + 
+		"titt,\r\n" + 
+		"tittie5,\r\n" + 
+		"tittiefucker,\r\n" + 
+		"titties,\r\n" + 
+		"tittyfuck,\r\n" + 
+		"tittywank,\r\n" + 
+		"titwank,\r\n" + 
+		"tosser,\r\n" + 
+		"turd,\r\n" + 
+		"tw4t,\r\n" + 
+		"twat,\r\n" + 
+		"twathead,\r\n" + 
+		"twatty,\r\n" + 
+		"twunt,\r\n" + 
+		"twunter,\r\n" + 
+		"v14gra,\r\n" + 
+		"v1gra,\r\n" + 
+		"vagina,\r\n" + 
+		"viagra,\r\n" + 
+		"vulva,\r\n" + 
+		"w00se,\r\n" + 
+		"wang,\r\n" + 
+		"wank,\r\n" + 
+		"wanker,\r\n" + 
+		"wanky,\r\n" + 
+		"whoar,\r\n" + 
+		"whore,\r\n" + 
+		"willies,\r\n" + 
+		"willy,\r\n" + 
+		"xrated,\r\n" + 
+		"xxx";
+}
--- src/filter/Filter.java
+++ src/filter/Filter.java
@@ -1,21 +1,14 @@
 package filter;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import existenceTrie.TST;
 
 public class Filter {
 	
-	private String filename;
 	private TST tst;
 	
-	public Filter(String filename) {
-		this.filename = filename;
+	public Filter() {
 		this.tst = new TST();
-		load(filename);
+		load();
 	}
 	
 	public boolean isSentenceSafe(String sentence) {
@@ -31,20 +24,15 @@ public class Filter {
 		return true;
 	}
 	
-	private void load(String filename) {
-		try {
-			String text = new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
-			String[] strings = text.split(",");
+	private void load() {
+		String[] strings = BadWords.bad_words.split(",");
+		
+		for(String s : strings) {
+			String current = s;
+			current = current.replace("\n", "").replace("\r", "");
+			current += existenceTrie.TST.END;
+			tst.insert(current);
 			
-			for(String s : strings) {
-				String current = s;
-				current = current.replace("\n", "").replace("\r", "");
-				current += existenceTrie.TST.END;
-				tst.insert(current);
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 	
